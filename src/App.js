91b5929807718c861935643a5d6bcafebb5bb50f
Andy Yao <andy.yao325@gmail.com>
2025-03-24 03:32:28 -0700

    initial sidebar commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { Sidebar, Menu, MenuItem } from 'react-pro-sidebar';
+import { Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Page1 from './Routes/page1';
+import Page2 from './Routes/page2';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="page1" element={<Page1/>} />
+        <Route path="page2" element={<Page2/>} />
+      </Routes>
+      <Sidebar>
+        <Menu>
+          <MenuItem component={<Link to="/page1"/>}> Page1 </MenuItem>
+          <MenuItem component={<Link to="/page2"/>}> Page2 </MenuItem>
+        </Menu>
+      </Sidebar>
+    </>
   );
 }
 
